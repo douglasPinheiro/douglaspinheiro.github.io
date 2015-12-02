3350d51716c5bdcca4e0e98128c103aecaf5eb1e
douglasPinheiro <douglaspinheiroti@gmail.com>
2015-12-02 00:44:24 -0200

    first layout

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('#progressHtml').progress({value:90, total:100});
+	$('#progressCss').progress({value:80, total:100});
+	$('#progressJS').progress({value:90, total:100});
+	$('#progressJquery').progress({value: 75, total:100});
+	$('#progressAngular').progress({value:85, total:100});
+	$('#progressDotNet').progress({value:95, total:100});
+	$('#progressRoR').progress({value:60, total:100});
+	$('#progressScrum').progress({value:95, total:100});
+});
