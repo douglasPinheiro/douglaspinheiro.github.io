b47394c6cb4727965bb59a75fe623ba23c34c0d6
douglasPinheiro <douglaspinheiroti@gmail.com>
2015-12-11 18:36:54 -0200

    add node ability

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,7 @@ $(function() {
 	$('#progressJS').progress({value:90, total:100});
 	$('#progressJquery').progress({value: 75, total:100});
 	$('#progressAngular').progress({value:85, total:100});
+	$('#progressNode').progress({value:80, total:100});
 	$('#progressDotNet').progress({value:95, total:100});
 	$('#progressRoR').progress({value:60, total:100});
 	$('#progressScrum').progress({value:95, total:100});
