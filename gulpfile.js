d920371b79cb66a83107c45ecebb039697643c3e
douglasPinheiro <douglaspinheiroti@gmail.com>
2015-12-13 20:34:28 -0200

    jade and minify

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,23 @@
  
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var jade = require('gulp-jade');
+var minifyCss = require('gulp-minify-css');
+ 
+gulp.task('jade', function() {
+  gulp.src('./views/*.jade')
+    .pipe(jade())
+    .pipe(gulp.dest('./'))
+});
  
 gulp.task('sass', function () {
-  gulp.src('./styles/sass/*.scss')
-    .pipe(sass().on('error', sass.logError))
+  gulp.src('./styles/sass/*.sass')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(minifyCss({compatibility: 'ie8'}))
     .pipe(gulp.dest('./styles/css'));
 });
  
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+gulp.task('watch', function () {
+  gulp.watch('./styles/sass/*.sass', ['sass']);
+  gulp.watch('./views/*.jade', ['jade']);
 });
