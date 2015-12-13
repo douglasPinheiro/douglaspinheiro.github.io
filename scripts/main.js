85266b3fd4a7855143e5d3d6a7513dd2dcbcabc3
douglasPinheiro <douglaspinheiroti@gmail.com>
2015-12-12 22:51:59 -0200

    correct scroll velocity

--- scripts/main.js
+++ scripts/main.js
@@ -28,10 +28,7 @@ $(function() {
 
 function backToTop(){
 	var offset = 300,
-	//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
-	offset_opacity = 1200,
-	//duration of the top scrolling animation (in ms)
-	scroll_top_duration = 700,
+
 	//grab the "back to top" link
 	$back_to_top = $('.cd-top');
 
@@ -45,7 +42,6 @@ function backToTop(){
 		event.preventDefault();
 		$('.parallax').animate({
 			scrollTop: 0 ,
-		 	}, scroll_top_duration
-		);
+	 	}, 1500);
 	});
 }
