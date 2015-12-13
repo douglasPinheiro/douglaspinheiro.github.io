4912812124b56ad8a6e86fb1c6a6fbdc01d53e2c
douglasPinheiro <douglaspinheiroti@gmail.com>
2015-12-13 14:16:21 -0200

    add sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
+'use strict';
+ 
 var gulp = require('gulp');
-
-gulp.task('default', function() {
-  // place code for your default task here
+var sass = require('gulp-sass');
+ 
+gulp.task('sass', function () {
+  gulp.src('./styles/sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./styles/css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
 });
