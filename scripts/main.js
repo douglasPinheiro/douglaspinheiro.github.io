43375dc06b2de1f0f3c89b9d56837ad4d9ecae0e
douglasPinheiro <douglaspinheiroti@gmail.com>
2015-12-15 01:34:28 -0200

    footer

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,12 @@
+'use strict'
+
+var resumePosition = $("#resume").offset().top,
+	abilitiesPosition = $("#abilities").offset().top,
+	certificatesPosition = $("#certificates").offset().top,
+	educationPosition = $("#education").offset().top,
+	portifolioPosition = $("#portfolio").offset().top,
+	contactPosition = $("#contact").offset().top;
+
 $(window).load(function() {
     $('#progressHtml').progress({value:90, total:100});
 	$('#progressCss').progress({value:80, total:100});
@@ -11,14 +20,63 @@ $(window).load(function() {
 
 	$('#scrollDown').on('click', function(event) {
         $('.parallax').animate({
-            scrollTop: $("#resume").offset().top
+            scrollTop: resumePosition
         }, 1500);
         return false;
 	});
 
 	$('#hire').on('click', function(event) {
         $('.parallax').animate({
-            scrollTop: $("#contact").offset().top
+            scrollTop: contactPosition
+        }, 1500);
+        return false;
+	});
+
+	$('#home-nav').on('click', function(event) {
+        $('.parallax').animate({
+            scrollTop: 0
+        }, 1500);
+        return false;
+	});
+
+	$('#resume-nav').on('click', function(event) {
+        $('.parallax').animate({
+            scrollTop: resumePosition
+        }, 1500);
+        return false;
+	});
+
+	$('#abilities-nav').on('click', function(event) {
+        $('.parallax').animate({
+            scrollTop: abilitiesPosition
+        }, 1500);
+        return false;
+	});
+
+	$('#certificates-nav').on('click', function(event) {
+        $('.parallax').animate({
+            scrollTop: certificatesPosition
+        }, 1500);
+        return false;
+	});
+
+	$('#education-nav').on('click', function(event) {
+        $('.parallax').animate({
+            scrollTop: educationPosition
+        }, 1500);
+        return false;
+	});
+
+	$('#portfolio-nav').on('click', function(event) {
+        $('.parallax').animate({
+            scrollTop: portifolioPosition
+        }, 1500);
+        return false;
+	});
+
+	$('#contact-nav').on('click', function(event) {
+        $('.parallax').animate({
+            scrollTop: contactPosition
         }, 1500);
         return false;
 	});
@@ -32,16 +90,12 @@ $(window).load(function() {
 
 function backToTop(){
 	var offset = 300,
-
-	//grab the "back to top" link
 	$back_to_top = $('.cd-top');
 
-	//hide or show the "back to top" link
 	$('.parallax').scroll(function(){
 		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
 	});
 
-	//smooth scroll to top
 	$back_to_top.on('click', function(event){
 		event.preventDefault();
 		$('.parallax').animate({
